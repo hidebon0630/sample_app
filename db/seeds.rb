90b48390e12ec2237f43fbcd8e38c74f80e040c9
hidebon0630 <hyagi0630@icloud.com>
2020-06-20 15:36:45 +0900

    usersコントローラーの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'ゲストユーザー',
+             email: 'guest@example.com',
+             password: 'password',
+             password_confirmation: 'password')
