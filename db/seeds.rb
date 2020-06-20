7eedd0744a8436e98c6bf05972e3c277e7975364
hidebon0630 <hyagi0630@icloud.com>
2020-06-20 17:19:01 +0900

    無限スクロールの実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,13 @@ User.create!(name: 'ゲストユーザー',
              email: 'guest@example.com',
              password: 'password',
              password_confirmation: 'password')
+
+100.times do |n|
+ name = Faker::Name.name
+ email = "sample-#{n + 1}@example.com"
+ password = 'password'
+ User.create!(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password)
+end
