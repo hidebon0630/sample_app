75ef42503cfb893cf733aa73cefeab875a9acadd
hidebon0630 <hyagi0630@icloud.com>
2020-06-21 16:21:51 +0900

    アンフォローボタンの修正と_user.html.erbの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[following followers]
   def index
     @users = User.page(params[:page]).per(20)
   end
@@ -6,5 +7,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     @posts = @user.posts.page(params[:page]).per(20)
+  end
+
+  def following
+    @title = "フォロー"
+    @user = User.find(params[:id])
+    @users = @user.following.page(params[:page]).per(10)
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user = User.find(params[:id])
+    @users = @user.followers.page(params[:page]).per(10)
+    render 'show_follow'
   end
 end
