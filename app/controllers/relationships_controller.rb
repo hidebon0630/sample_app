ede12e6ebac275813653451f6726fad25289466e
hidebon0630 <hyagi0630@icloud.com>
2020-06-21 16:28:30 +0900

    フォロー機能の修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,7 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   def create
-    user = User.find(params[:id])
+    user = User.find(params[:followed_id])
     current_user.follow(user)
     redirect_to user
   end
