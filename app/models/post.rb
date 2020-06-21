2291de39abea9faef45fab3d6d05fccb7d3aeac9
hidebon0630 <hyagi0630@icloud.com>
2020-06-21 13:54:54 +0900

    画像の拡張子を指定

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :image_size
+
+  private
+    def image_size
+      if image_size > 5.megabytes
+        errors.add(:image, "should be less than 5MB")
+      end
+    end
 end
