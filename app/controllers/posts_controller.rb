ef69f9654df47469c8ee49760a8873e1bf18ada0
hidebon0630 <hyagi0630@icloud.com>
2020-06-21 12:58:53 +0900

    destroyアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def index
     @posts = Post.page(params[:page]).per(20)
@@ -24,11 +25,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
-
+    @post.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to request.referrer || root_url
   end
 
   private
     def post_params
       params.require(:post).permit(:content)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
