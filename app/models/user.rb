d659698e38a4d67a6fb75b37e500ffde2e566f61
hidebon0630 <hyagi0630@icloud.com>
2020-06-21 14:32:57 +0900

    関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
