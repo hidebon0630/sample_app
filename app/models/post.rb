0d1f07a4f89898055aeb52a919ce2d13885bd0a9
hidebon0630 <hyagi0630@icloud.com>
2020-06-21 13:05:15 +0900

    postモデルに画像を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :image, imageUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_105758) do
+ActiveRecord::Schema.define(version: 2020_06_21_040343) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
