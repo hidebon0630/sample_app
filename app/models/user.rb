c55c5e3f31ef56d153fa230a424d620107bb389a
hidebon0630 <hyagi0630@icloud.com>
2020-06-22 14:42:51 +0900

    annotateを使ってスキーマを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  admin_flg              :boolean
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  name                   :string(255)      default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
