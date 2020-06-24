bbed5491bd4af0bb338653c1918ffb1617f590ae
hidebon0630 <hyagi0630@icloud.com>
2020-06-24 14:31:53 +0900

    簡易的なグラフの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
