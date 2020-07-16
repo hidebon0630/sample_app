bb7622c151eb892bf5ed747925d366766f053fda
hidebon0630 <hyagi0630@icloud.com>
2020-07-16 17:18:04 +0900

    database-cleanerの削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,17 +34,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
