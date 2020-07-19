18e04a49825491d2f20ee1f69e442d64f9abfa7c
hidebon0630 <hyagi0630@icloud.com>
2020-07-20 06:26:01 +0900

    DMのAjax化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,15 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
-      flash[:notice] = 'メッセージを送信しました。'
-      redirect_to "/rooms/#{@message.room_id}"
-    else
-      flash[:notice] = 'メッセージに失敗しました。'
-      redirect_back(fallback_location: root_path)
+      if @message.save
+        @message = Message.new
+        @room = Room.find(params[:message][:room_id])
+        @messages = @room.messages
+        @entries = @room.entries
+        respond_to do |format|
+          format.js
+        end
+      end
     end
   end
 end
