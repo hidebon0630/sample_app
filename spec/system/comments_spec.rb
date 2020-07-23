ff2510a19578ffc46a43967336bbba243fd2a42d
hidebon0630 <hyagi0630@icloud.com>
2020-07-24 03:54:58 +0900

    システムスペックの追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Comments', type: :system do
   scenario 'コメント' do
-    user = FactoryBot.create(:user)
-    post = FactoryBot.create(:post)
+    user = create(:user)
+    option = create(:option)
 
     sign_in user
     visit posts_path
-    click_link href: post_path(post)
-    expect(page).to have_button '回答'
+    click_link href: post_path(option.post)
+    expect(current_path).to eq post_path(option.post)
+    choose 'あいうえお'
+    expect(page).to have_checked_field 'あいうえお'
+    click_button '回答'
+    expect(current_path).to eq post_votes_path(option.post)
+    expect(page).to have_content '回答者数'
+    #expect do
+    #  fill_in 'コメント', with: 'コメント'
+    #  click_button '送信する'
+    #end.to change(user.comments :count).by(1)
   end
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,27 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :system do
   scenario '新規投稿' do
-    user = create(:user, :sample)
+    user = create(:user)
     sign_in user
 
     visit posts_path
 
-    # expect do
-    link = find('.new-post-link')
-    expect(link[:href]).to eq new_post_path
-    link.click
-    fill_in '質問', with: '質問'
-    attach_file 'post[image]', "#{Rails.root}/spec/fixtures/sample1.png"
-    # click_button '回答'
-
-    # expect(page).to have_content '投稿が完了しました'
-    # end.to change(user.posts, :count).by(1)
+    expect do
+      link = find('.new-post-link')
+      expect(link[:href]).to eq new_post_path
+      link.click
+      fill_in '質問', with: '質問'
+      fill_in '回答項目', with: 'あいうえお'
+      attach_file 'post[image]', "#{Rails.root}/spec/fixtures/sample1.png"
+      click_button '投稿'
+    end.to change(user.posts, :count).by(1)
   end
 
   scenario '投稿詳細' do
-    user = create(:user, :sample)
-    post = create(:post, user: user)
-    sign_in user
-    visit post_path(post)
+    post = create(:post)
+
+    sign_in post.user
+    visit posts_path
+    click_link href: post_path(post)
+    expect(current_path).to eq posts_path
+
+    #other_user = create(:user)
+    #post = create(:post)
+#
+    #sign_in user
+    #visit posts_path
+    #click_link href: post_path(post)
+    #expect(current_path).to eq post_path(post)
+    #click_button '回答'
   end
 end
