68235a9d6fb845b236e5eacc7f4ed185ae53ae46
hidebon0630 <hyagi0630@icloud.com>
2020-08-10 01:57:35 +0900

    ランディングページの完成

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,5 +1,5 @@
 AssetSync.configure do |config|
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = 'AWS'
   config.fog_directory = Rails.application.credentials.aws[:s3_bucket]
   config.aws_access_key_id = Rails.application.credentials.aws[:access_key_id]
   config.aws_secret_access_key = Rails.application.credentials.aws[:secret_access_key]
